6050c0fd109ff50e3e0e980f5aabd6c187c1bf4b
tugas <aldayanuar6@gmail.com>
2024-10-03 20:32:46 +0700

    masukan percobaan 1

---
+++ Pemilihan2Percobaan125.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan125 {
+public static void main(String[] args) {
+    Scanner inputAbsen = new Scanner(System.in);
+    System.out.println("Masukan tahun: ");
+    int tahun = inputAbsen.nextInt();
+    if ((tahun % 4) == 0) {
+        if((tahun % 100) != 0);
+        System.out.println("Tahun Kabisat: ");
+    } else
+    System.out.println("Bukan Tahun Kabisat");
+}
+}
